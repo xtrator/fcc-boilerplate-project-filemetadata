480bb93a308da469eefaece19dbdc8061ae0ffeb
Alexander Luna <alexfox_2000@hotmail.com>
2023-02-13 16:13:08 -0500

    implement post /api/fileanalyse endpoint

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var cors = require("cors");
 require("dotenv").config();
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 var app = express();
 
@@ -9,6 +12,13 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  let name = req.file.originalname;
+  let type = req.file.mimetype;
+  let size = req.file.size;
+  res.json({ name, type, size });
 });
 
 const port = process.env.PORT || 3000;
